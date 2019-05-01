e57c4bfdd0391a855780f93d409fadf4a0534180
Sriram Velamur <sriram.velamur@gmail.com>
2019-05-01 22:14:13 +0530

    Moves examples to a new path and updates example server code
    1. Updates base route/controller for template renderer flow.
    2. Adds API route example for regex based query matching.
    
    TODO: Add server side renderer flow to use data in template renderer

---
+++ examples/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { Server } = require('../http-server');
+
+function UserController(request, response) {
+  response.respond(200, request.query);
+}
+
+function RootController(request, response) {
+  response.renderTemplate('templates/index.html');
+}
+
+Server
+  .create({
+    '/': RootController,
+    '/api/users/(?<username>\\w+)\/?': UserController
+  })
+  .setResponseType('html')
+  .start(3000);
