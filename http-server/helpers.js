4f97fe074195971d2c4b4699e12434b6a38dc626
Sriram Velamur <sriram.velamur@gmail.com>
2019-06-07 23:19:08 +0530

    Adds a Promise based helper to load templates and render variables

---
+++ http-server/helpers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { readFile } = require('fs');
+
+
+const renderTemplateWithData = (path, data) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, page) => {
+      if (err) { return reject(err); }
+      data = data || {};
+      page = page.toString();
+      Object.keys(data).map(key => {
+        let val = data[key];
+        let matcher = new RegExp(`{{\\s*${key}\\s*}}`, 'gmi');
+        let match = matcher.exec(page);
+        if (match) {
+          page = page.replace(matcher, val);
+        }
+      });
+      resolve(page);
+    });
+  });
+};
+
+
+module.exports = { renderTemplateWithData };
